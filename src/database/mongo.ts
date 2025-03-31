32c8b7c939a6215955d874c445ad1e4bb6c1493b
LucasVince <lucashenriquevince1714@gmail.com>
2025-03-30 21:39:57 -0300

    fix: finished api

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,18 +1,20 @@
-import { Db, MongoClient } from 'mongodb';
+import { Db, MongoClient } from "mongodb";
 
 export const mongoClient = {
-    client: undefined  as MongoClient | undefined,
-    db: undefined as Db | undefined,
+  client: undefined as MongoClient | undefined,
+  db: undefined as Db | undefined,
 
-    async connect(): Promise<void> {
-        const url = process.env.MONGO_URL || 'localhost:27017';
-        const username = process.env.MONGO_USERNAME;
-        const password = process.env.MONGO_PASSWORD;
+  async connect(): Promise<void> {
+    const url = process.env.MONGO_URL || "localhost:27017";
+    const username = process.env.MONGO_USERNAME;
+    const password = process.env.MONGO_PASSWORD;
 
-        const client = new MongoClient(url, { auth: { username, password } });
-        const db = client.db('users-db');
+    const client = new MongoClient(url, { auth: { username, password } });
+    const db = client.db("users-db");
 
-        this.client = client;
-        this.db = db;
-    }
+    this.client = client;
+    this.db = db;
+
+    console.log('connected successfully to db');
+  },
 };
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import express from "express";
-import { config } from "dotenv";
-import {getUsersController} from './controllers/get-users/get-users'
-import {mongoGetusersRepository} from './controllers/repositories/get-users/mongo-get-users'
+import dotenv from "dotenv";
+import { getUsersController } from "./controllers/get-users/get-users";
+import { mongoGetusersRepository } from "./controllers/repositories/get-users/mongo-get-users";
+import { mongoClient } from "./database/mongo";
 
-const app = express();
+const main = async () => {
+  dotenv.config();
 
-config();
-app.use(express.json());
-app.get("/users", async (req, res) => {
-  const MongoGetusersRepository = new mongoGetusersRepository;
-  const GetUsersController = new getUsersController(MongoGetusersRepository);
+  const app = express();
 
-  const response = await GetUsersController.handle();
+  await mongoClient.connect();
 
-  res.send(response.body).status(response.statusCode);
-});
+  app.use(express.json());
 
-app.listen(8080, () => console.log("Server running on port 8080"));
+  app.get("/users", async (req, res) => {
+    const MongoGetusersRepository = new mongoGetusersRepository();
+    const GetUsersController = new getUsersController(MongoGetusersRepository);
+
+    const response = await GetUsersController.handle();
+
+    res.send(response.body).status(response.statusCode);
+  });
+
+  app.listen(8080, () => console.log("Server running on port 8080!!"));
+};
+
+main();
